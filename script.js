b056b86776c8cbb097886fef64c6a5388f801d3a
donggyu <donggyu258@gmail.com>
2022-04-12 20:19:34 -0700

    display updates after every button click, except the evaluate button

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 // GLOBAL VARIABLES
+let displayValue = "";
 
+const MAX_DISPLAY_LENGTH = 15;
+const DISPLAY_CONTAINER = document.querySelector(".display-container");
+const KEYPAD_CONTAINER = document.querySelector(".keypad-container");
 
+const ALL_KEYS = document.querySelectorAll(".key");
+const NUMBER_KEYS = document.querySelectorAll(".number");
+const OPERAND_KEYS = document.querySelectorAll('.operand');
+const EQUALS_KEY = document.querySelector('.evaluate');
+const CLEAR_KEY = document.querySelector('.clear');
 
 
 // FUNCTIONS
@@ -20,6 +29,12 @@ function divide(num1, num2) {
     return num1 / num2;
 }
 
+// [12] [+] [7] [-] [5] [*] [3]
+
+// [12 + 7] - 5 * 3
+//     [19 - 5] * 3
+//         [14 * 3]
+//             [42]
 function operate(operator, num1, num2) {
     switch (operator) {
         case "+":
@@ -35,6 +50,48 @@ function operate(operator, num1, num2) {
     }
 }
 
+function populateDisplay(keyInput) {
+    if (displayValue.length < MAX_DISPLAY_LENGTH) {
+        displayValue += keyInput;
+        if (displayValue.length > MAX_DISPLAY_LENGTH) {
+            displayValue = displayValue.slice(0, MAX_DISPLAY_LENGTH);
+        }
+    }
+}
+
+function updateDisplay() {
+    populateDisplay(this.textContent);
+    DISPLAY_CONTAINER.textContent = displayValue;
+}
+
+function resetDisplay() {
+    displayValue = '';
+    DISPLAY_CONTAINER.textContent = displayValue;
+}
+
+function hookUpButtons() {
+    NUMBER_KEYS.forEach(key => key.addEventListener('click', updateDisplay));
+    OPERAND_KEYS.forEach(key => key.addEventListener('click', updateDisplay));
+    CLEAR_KEY.addEventListener('click', resetDisplay);
+    // add event listener for EQUALS_KEY to evaluate current expression
+}
+
+hookUpButtons();
+
+
+
+/**
+ * 1. when user presses a button, display should be updated 
+ * 2. "correct" order should be: <digit> ... <digit> | <operand> <digit> ... <digit>
+ * 3. when CLR pressed, clear the entire display
+ * 4. when = is pressed, evaluate the expression on the display
+ * 5. ORDER OF OPERATIONS IGNORED (FOR NOW)
+ * 
+ * - calculations always start with a number
+ * - if operands are used, they MUST be followed by another number
+ * - if number is too big to fit on display, it will be truncated (to MAX_DISPLAY_LENGTH digits)
+ * - if equals pressed after a "correct" order of operations, evaluate the expression on screen 
+ */
 
 
 
